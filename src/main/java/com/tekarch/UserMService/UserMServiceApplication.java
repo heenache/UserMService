51523395b84853279f4860f8117dc582d95b05fe
Heena C <heenac@tekarch.com>
2025-01-02 23:15:31 -0600

    Initial commit

---
+++ src/main/java/com/tekarch/UserMService/UserMServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tekarch.UserMService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tekarch/UserMService/UserMServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tekarch.UserMService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
